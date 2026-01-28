356b73c023d70b2b28fddfa8635ee649539aa81e
RyanL4004 <rlang5@unh.newhaven.edu>
2026-01-28 11:58:55 -0500

    P1 is pretty much done, just needs to be printed into a file according to instruction document.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-vector<string> arguments;
+    cout << "---------------------------------" << endl;
 
-for (int x = 1; x < argc; x++) {
-    arguments.push_back(argv[x]);
-}
-    cout << "All # of Arguments: " << arguments.size() << endl;
-    for (size_t y = 0; y < arguments.size(); y++) {
-        cout << "Argument" << ":" << arguments[y] << endl;
+    string command = argv[0];
+    if (command.rfind("./", 0) == 0) {
+        command.erase(0, 2);
+    }
+
+    cout << left << setw(10) << "command" << command << endl;
+
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+
+        if (!arg.empty() && arg[0] == '-') {
+            arg.erase(0, 1);
+            cout << left << setw(10) << "switch" << arg << endl;
+        }
+        else {
+            cout << left << setw(10) << "argument" << arg << endl;
+        }
     }
 
     return 0;
