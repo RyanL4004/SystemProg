872ce2bad8b2c850c9926ea5a1c6cb66d2e7fcef
RyanL4004 <rlang5@unh.newhaven.edu>
2026-01-28 13:03:22 -0500

    Everything is done. Run it according to the instructions, and make sure everything is filling the files as needed. Then, add your feedback in a .txt file.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
+    ofstream myOut;
+    myOut.open("P1_LangMather.txt", ios::out | ios::app);
+
+    if (!myOut) {
+        cerr << "Error: Could not open the file." << endl;
+        return 1;
+    }
+
     cout << "---------------------------------" << endl;
+    myOut << "---------------------------------" << endl;
 
     string command = argv[0];
     if (command.rfind("./", 0) == 0) {
@@ -14,6 +24,7 @@ int main(int argc, char *argv[]) {
     }
 
     cout << left << setw(10) << "command" << command << endl;
+    myOut << left << setw(10) << "command" << command << endl;
 
     for (int i = 1; i < argc; i++) {
         string arg = argv[i];
@@ -21,11 +32,15 @@ int main(int argc, char *argv[]) {
         if (!arg.empty() && arg[0] == '-') {
             arg.erase(0, 1);
             cout << left << setw(10) << "switch" << arg << endl;
+            myOut << left << setw(10) << "switch" << arg << endl;
         }
         else {
             cout << left << setw(10) << "argument" << arg << endl;
+            myOut << left << setw(10) << "argument" << arg << endl;
         }
     }
+
+    myOut.close();
 
     return 0;
 }
